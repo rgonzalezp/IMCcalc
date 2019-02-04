d70f4c989db8b4c8eaa675b507cbad3384016bf8
Federico Gonzalez <rgonzalezp1115@gmail.com>
2019-02-04 11:25:07 -0500

    Initial Commit

---
+++ IMCcalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IMCcalc
+//
+//  Created by Federico Gonzalez on 2/4/19.
+//  Copyright © 2019 Ricardo Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
